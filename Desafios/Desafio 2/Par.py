15fc3155af2e734bc1f8047be6c5cab89e313d99
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2021-09-08 18:08:48 -0300

    Add files via upload

---
+++ Desafios/Desafio 2/Par.py
@@ -0,0 +1,15 @@
+_author_ = "Saldaño"
+
+def par(n):
+    if n % 2 == 0:
+        return True
+    return False
+
+def validate(inf):
+    n = inf
+    while n <= inf:
+        n = int(input("Ingrese un numero: "))
+        if n <= inf:
+            print("Se pide un numero mayor a cero.")
+    return n
+
